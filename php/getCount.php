16d533281f30d89c22bb1c29eadcd402506d55c1
akturis <akturis@gmail.com>
2016-12-27 14:48:14 +0300

    Update getCount.php

--- php/getCount.php
+++ php/getCount.php
@@ -4,7 +4,8 @@ require_once("config.php");
 $conn = mysql_connect($config['db']['host'],$config['db']['username'],$config['db']['password']) or die($config['db']['errormsg'].mysql_error());
 mysql_select_db($config['db']['dbname']) or die($config['db']['errormsg'].mysql_error());
 
-$count = mysql_query('SELECT DATE_FORMAT(logon,\'%e %b %Y\') dat, COUNT(*) count FROM (SELECT DISTINCT date(logonDateTime) logon, characterId FROM `logons` WHERE logonDateTime>(CURDATE() - INTERVAL 12 MONTH)) dist GROUP BY logon ORDER BY COUNT(*) DESC LIMIT 1');
+$corpid = $_GET['corpid'];
+$count = mysql_query('SELECT DATE_FORMAT(logon,\'%e %b %Y\') dat, COUNT(*) count FROM (SELECT DISTINCT date(logonDateTime) logon, characterId FROM `logons` WHERE logonDateTime>(CURDATE() - INTERVAL 12 MONTH)) dist LEFT JOIN characters c ON c.id = characterid WHERE c.corporationid = '.$corpid.' GROUP BY logon ORDER BY COUNT(*) DESC LIMIT 1');
 if ($count =  mysql_fetch_assoc($count))
 echo "          <h2>Top online</h2>
           <h1>{$count['count']}</h1>
