57a05b94abd141c410503a2ee5f13a756dc06474
akturis <akturis@gmail.com>
2016-12-27 14:53:38 +0300

    Update super.php

--- php/super.php
+++ php/super.php
@@ -1,6 +1,7 @@
 <?php 
     require("config.php"); 
-    if (!isset($_SESSION)) { session_start(); }
+    session_start();
+//    if (!isset($_SESSION)) { session_start(); }
     if (!empty($_SESSION['user'])) {
 
             echo '      
@@ -41,4 +42,8 @@
         </ul>
       </div>";    
     }
+//    if (empty($_SESSION['CCP']['user']['CharacterID'])&&empty($_SESSION['user'])) {
+//        echo "<a href='#' id='ssoSelectButton'><img alt='EVE SSO Login Buttons Small White' src='https://images.contentful.com/idjq7aai9ylm/18BxKSXCymyqY4QKo8KwKe/c2bdded6118472dd587c8107f24104d7/EVE_SSO_Login_Buttons_Small_White.png?w=195&amp;h=30' data-pin-nopin='true'></a>";    
+//    }
+
 ?> 
