3dae71a6b31cf93c95362509f7496b1aeccc2b54
akturis <akturis@gmail.com>
2016-12-27 14:51:57 +0300

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -3,6 +3,8 @@
     session_start();//session is a way to store information (in variables) to be used across multiple pages.  
     unset($_SESSION['user']);
     unset($_SESSION['CCP']['user']);
+    unset($_SESSION['CCP']['user']['CharacterID']);
+    unset($_SESSION['director']);
     session_destroy();    
 //    header("Location: ../index.html"); 
 //    die("Redirecting to: index.html");
